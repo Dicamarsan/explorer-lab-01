d5e6f8c7f37de381e4ad82baf97103a554f5acc6
Diana Martinez <dicamarsan@hotmail.com>
2022-10-19 15:16:48 -0300

    trocando green para black e blue para gray, projeto-javascript para projeto-base, acrecentou projeto-base, trocando uuid:^9.0.0 para imask: ^6.4.3 e nodemon: ^2.0.20, vite: ^3.1.8 para vite: ^3.1.0, acrecentou imask: ^6.4.3, americanexpress para visa

--- src/main.js
+++ src/main.js
@@ -1,25 +1,36 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 
 const ccLogo = document.querySelector("cc-logo span:nth-child(2) img")
 
-
-
 function setCardType(type) {
-const colors = {
-  visa: ["#436D99", "#2D57F2"],
-  mastercard: ["#DF6F29", "#C69347"],
-  americanexpress: ["#29AA9B", "#9F1280"],
-  default: ["black", "gray"],
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    americanexpress: ["#29AA9B", "#9F1280"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-ccBgColor01.setAttribute("fill", colors[type] [0])
-ccBgColor02.setAttribute("fill", colors[type][1])
-ccLogo.setAttribute("src", `cc-${type}.svg`)
-}
-
-setCardType("americanexpress")
+setCardType("visa")
 
 globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#espiration-date")
+const expirationDatePattern = {
+  mask: "00{/}00"
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
